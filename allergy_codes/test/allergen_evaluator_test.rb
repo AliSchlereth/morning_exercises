93cc3fd66fe80b6474bd0eaaad9c4298eb481324
Ali Schlereth <aschlere@sbcglobal.net>
2017-05-17 09:54:13 -0600

    Calculate combined score working multiple numbers

--- allergy_codes/test/allergen_evaluator_test.rb
+++ allergy_codes/test/allergen_evaluator_test.rb
@@ -1,6 +1,7 @@
 require "minitest/autorun"
 require "minitest/pride"
 require './lib/allergen_evaluator'
+require "pry"
 
 class AllergenEvaluatorTest < Minitest::Test
 
@@ -14,8 +15,49 @@ class AllergenEvaluatorTest < Minitest::Test
   def test_return_eggs_and_peanuts_for_score_of_3
     evaluator = AllergenEvaluator.new
     allergens = evaluator.assess_allergy_score(3)
+    included = allergens.include?("eggs") && allergens.include?("peanuts")
 
-    assert_equal ["eggs", "peanuts"], allergens
+    assert included
+  end
+
+  def test_combined_score_returns_highest_code_less_than_score
+    evaluator = AllergenEvaluator.new
+
+    refute evaluator.allergen_codes.include?("8")
+
+    evaluator.calculate_combined_score(9)
+
+    assert evaluator.allergen_codes.include?("8")
+    assert evaluator.allergen_codes.include?("1")
+  end
+
+  def test_calculate_combined_score_19
+    evaluator = AllergenEvaluator.new
+
+    refute evaluator.allergen_codes.include?("8")
+
+    evaluator.calculate_combined_score(19)
+
+    assert evaluator.allergen_codes.include?("16")
+    assert evaluator.allergen_codes.include?("1")
+    assert evaluator.allergen_codes.include?("2")
+  end
+
+  def test_calculate_combined_score_39
+    evaluator = AllergenEvaluator.new
+
+    refute evaluator.allergen_codes.include?("8")
+
+    evaluator.calculate_combined_score(39)
+
+    assert evaluator.allergen_codes.include?("32")
+    assert evaluator.allergen_codes.include?("4")
+    assert evaluator.allergen_codes.include?("2")
+    assert evaluator.allergen_codes.include?("1")
+  end
+
+  def test_allergy_score
+
   end
 
 end
