9dd838a68b2822a26280b30228f0ab474785d524
Ali Schlereth <aschlere@sbcglobal.net>
2017-05-24 09:42:59 -0600

    Analyze lines with reduce

--- swift_stats/lib/lyrics_analysis.rb
+++ swift_stats/lib/lyrics_analysis.rb
@@ -1,7 +1,41 @@
 class LyricsAnalysis
 
+  def word_count(filename)
+    lyric_lines = read_file(filename)
+    collect_each_unique_word(lyric_lines)
+  end
 
-  
+  def read_file(filename)
+    lyrics = []
+    File.read(filename).each_line do |line|
+      lyrics << line.chomp
+    end
+    lyrics
+  end
+
+  def collect_each_unique_word(lyric_lines)
+    thing = lyric_lines.reduce({}) do |result, line|
+      result.merge(analyze_each_word(line))
+    end
+    binding.pry
+  end
+
+  def analyze_each_word(line)
+    # line.split(" ").group_by do |word|
+    #   word.downcase
+    # end
+    line.split(" ").reduce({}) do |result, word|
+      if result[word.downcase]
+        result[word.downcase] += 1
+      else
+        result[word.downcase] = 1
+      end
+      result
+    end
+  end
+
+
+
 
 
 end
